705216a19257307171c97f8e5b88e807a1777764
Philip Welber <prwelber@gmail.com>
2017-02-27 21:27:38 -0500

    first commit

---
+++ index.ios.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'https://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/sample1_l.jpg'}},
+  {title: 'Some Movie', year: '2016', posters: {thumbnail: 'https://rescuethepresent.net/tomandjerry/files/2016/09/free-movie-film-poster-the_dark_knight_movie_poster.jpg'}}
+];
+
+export default class AwesomeProject extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+        <View style={styles.container}>
+          <Image
+            source={{uri: 'https://rescuethepresent.net/tomandjerry/files/2016/09/free-movie-film-poster-the_dark_knight_movie_poster.jpg'}}
+            style={styles.thumbnail}
+          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
